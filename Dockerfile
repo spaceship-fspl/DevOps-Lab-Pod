f4cc67889ac0b74dcc4d5ffa1ab32133d800f773
Adrian Hannelly <abh@hannelly.org>
2019-10-23 12:45:18 +1100

    init

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:alpine as builder
+RUN mkdir /go/src/app
+ADD . /go/src/app/
+WORKDIR /go/src/app
+RUN go build -o app .
+RUN chmod +x app
+
+FROM alpine
+COPY --from=builder /go/src/app/app /app
+RUN apk update && \
+    apk add ca-certificates && \
+    update-ca-certificates && \
+    rm -rf /var/cache/apk/* && \
+    apk add curl
+
+CMD ["/app"]
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	loc, _ := time.LoadLocation("EST")
+	t := time.Now().In(loc).Format(time.RFC3339)
+	fmt.Fprintf(w, "Please submit a screen shot of this page with your solution %s! %v", r.URL.Path[1:], t)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
